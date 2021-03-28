9f1fd556b3a6661def121e3977b1a8b855491f54
Diegofergamboa <diegofergamboa@hotmail.es>
2021-03-28 23:26:17 +0000

    done

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+
+import random
+print('Welcome to the Rock, scissors and paper game.')
+username = input('what is your name? ')
+choice_user = input('¿scissors, rock or paper? ')
+choice_pc = random.choice(['rock', 'scissors', 'paper'])
+
 rock = '''
     _______
 ---'   ____)
@@ -25,3 +32,23 @@ scissors = '''
 ---.__(___)
 '''
 
+if choice_user == 'scissors' and choice_pc == 'paper' :
+	print('Machine choise ' + choice_pc)
+	print(username + ' won!')
+elif choice_user == 'scissors' and choice_pc == 'rock' :
+	print('Machine choise ' + choice_pc)
+	print(username +' lose!')
+elif choice_user == 'paper' and choice_pc == 'scissors' :
+	print('Machine choise ' + choice_pc)
+	print(username +' lose!')
+elif choice_user == 'paper' and choice_pc == 'rock' :
+	print('Machine choise ' + choice_pc)
+	print('{username} won!')
+elif choice_user == 'rock' and choice_pc == 'scissors' :
+	print('Machine choise ' + choice_pc)
+	print(username + ' won!')
+elif choice_user == 'rock' and choice_pc == 'scissors' :
+	print('Machine choise ' + choice_pc)
+	print(username + ' won!')
+else :
+	print('drawm')
